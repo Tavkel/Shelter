dc169a3cd7b15e6babe72b9c872dca1c1c4a8950
Grisha <guruwolf8@gmail.com>
2023-10-12 16:31:23 +0300

    create model Report and change contacts.sql

--- src/main/resources/liquibase/changeSets/contacts.sql
+++ src/main/resources/liquibase/changeSets/contacts.sql
@@ -4,10 +4,12 @@
 
 CREATE TABLE contact (
 id BIGINT NOT NULL,
+owner_id BIGINT NOT NULL,
 phone INT NOT NULL,
 telegram_chat_id BIGINT NOT NULL,
 email VARCHAR(255) NOT NULL,
 address VARCHAR(255) NOT NULL,
 comment TEXT NOT NULL,
+CONSTRAINT "idPK" PRIMARY KEY (owner_id),
 CONSTRAINT "FK_contact_owner" FOREIGN KEY (id) REFERENCES owner(id)
 );
