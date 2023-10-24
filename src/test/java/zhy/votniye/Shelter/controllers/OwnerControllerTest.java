6436ce57a48f8bca9392da4a1c5045f2fc68ebe8
warlocktony <antonz3594@gmail.com>
2023-10-24 17:06:52 +0300

    trouble with testing method update in OwnerControllerTest

--- src/test/java/zhy/votniye/Shelter/controllers/OwnerControllerTest.java
+++ src/test/java/zhy/votniye/Shelter/controllers/OwnerControllerTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import zhy.votniye.Shelter.models.DTO.ContactDTO;
@@ -13,6 +15,8 @@ import zhy.votniye.Shelter.models.DTO.OwnerDTO;
 import zhy.votniye.Shelter.models.DTO.PetDTO;
 import zhy.votniye.Shelter.models.enums.Status;
 import zhy.votniye.Shelter.repository.OwnerRepository;
+import zhy.votniye.Shelter.utils.mappers.OwnerMapper;
+import zhy.votniye.Shelter.utils.mappers.PetMapper;
 
 import java.time.LocalDateTime;
 
@@ -37,10 +41,11 @@ public class OwnerControllerTest {
     byte[] photo = new byte[1];
     ContactDTO c = new ContactDTO(2L,4257457435745L,
             "ohohohho","aaaaaa","kuku");
-    OwnerDTO o = new OwnerDTO(1L,"ivan","ivanovich",
-            "shulc",c, Status.OwnerStatus.AWAITING_CONFIRMATION);
+    OwnerDTO o = new OwnerDTO(0L,"ivan","ivanovich",
+            "shulc");
 
-    PetDTO p = new PetDTO(0L,"f","3w",3F, LocalDateTime.now(),photo,
+
+    PetDTO p = new PetDTO(0L,"f",true,"3w",3F, LocalDateTime.now(),photo,
             null,"dsgf","dsf",null);
 
     @Test
@@ -51,5 +56,49 @@ public class OwnerControllerTest {
         assertEquals(HttpStatus.OK, ownerDTOResponseEntity.getStatusCode());
         assertEquals(o, ownerDTOResponseEntity.getBody());
     }
+
+    @Test
+    void  read__returnStatus200AndOwner() {
+
+        var res = ownerRepository.save(OwnerMapper.toOwner(o));
+
+        ResponseEntity<OwnerDTO> ownerDTOResponseEntity =
+                restTemplate.getForEntity(
+                        "http://localhost:" + port + "/owner/" + res.getId(), OwnerDTO.class);
+        assertEquals(HttpStatus.OK, ownerDTOResponseEntity.getStatusCode());
+        assertEquals(OwnerMapper.fromOwner(res), ownerDTOResponseEntity.getBody());
+    }
+
+        @Test
+        void update_ownerInDB_returnStatus200AndOwner() {
+            var original = ownerRepository.save(OwnerMapper.toOwner(o));
+
+            var updated = new OwnerDTO(original.getId(),"ivanus","ivanovich",
+                    "shulc");
+
+            ResponseEntity<OwnerDTO> update = restTemplate.exchange(
+                    "http://localhost:" + port + "/owner",
+                    HttpMethod.PUT, new HttpEntity<>(updated), OwnerDTO.class);
+
+            assertEquals(HttpStatus.OK, update.getStatusCode());
+            assertEquals(updated, update.getBody());
+        }
+
+    @Test
+    void delete_ownerInDB_returnStatus200() {
+
+        var res = ownerRepository.save(OwnerMapper.toOwner(o));
+
+
+
+        ResponseEntity<OwnerDTO> delete = restTemplate.exchange(
+                "http://localhost:" + port + "/owner/" + res.getId(),
+                HttpMethod.DELETE, null, OwnerDTO.class
+        );
+        assertEquals(HttpStatus.OK, delete.getStatusCode());
+        assertEquals(OwnerMapper.fromOwner(res), delete.getBody());
+    }
+
+
 }
 
